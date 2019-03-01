9e0cdeb0599edb8af4c482ee6730d2e9655715a2
Jose Santacruz <jose@kushkipagos.com>
2019-03-01 18:01:44 -0500

    add third parameter

--- src/aws/aws_handler.js
+++ src/aws/aws_handler.js
@@ -2,5 +2,5 @@ const awsServerlessExpress = require('aws-serverless-express')
 
 module.exports = (expressApp)=>{
     const server = awsServerlessExpress.createServer(expressApp)
-    return (event, context) => { awsServerlessExpress.proxy(server, event, context) }
+    return (event, context, cb) => { awsServerlessExpress.proxy(server, event, context) }
 }
